124653137cf118b5873ecf96d517dbcd504215d1
George Onalo <onalotech7@gmail.com>
2023-03-31 08:31:37 +0100

    created rds module

---
+++ rds/main.tf
@@ -0,0 +1,29 @@
+# create database subnet group
+resource "aws_db_subnet_group" "database_subnet_group" {
+  name        = "${var.project_name}-${var.environment}-database-subnets"
+  subnet_ids  = [var.private_data_subnet_az1_id, var.private_data_subnet_az2_id]
+  description = "subnets for database instance"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-database-subnets"
+  }
+}
+
+# get information about a database snapshot
+data "aws_db_snapshot" "latest_db_snapshot" {
+  db_snapshot_identifier = var.database_snapshot_identifier
+  most_recent            = true
+  snapshot_type          = manual
+}
+
+# launch an rds instance from a database snapshot
+resource "aws_db_instance" "database_instance" {
+  instance_class         = var.database_instance_class
+  skip_final_snapshot    = true
+  availability_zone      = var.availability_zone_1
+  identifier             = var.database_instance_identifier
+  snapshot_identifier    = data.aws_db_snapshot.latest_db_snapshot.id
+  db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
+  multi_az               = var.multi_az_deployment
+  vpc_security_group_ids = [var.database_security_group_id]
+}
---
+++ rds/variables.tf
@@ -0,0 +1,11 @@
+# rds variables
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "database_snapshot_identifier" {}
+variable "database_instance_class" {}
+variable "availability_zone_1" {}
+variable "database_instance_identifier" {}
+variable "multi_az_deployment" {}
+variable "database_security_group_id" {}
