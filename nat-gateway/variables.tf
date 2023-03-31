a974193f875cdf0dcfb9d9eaca88798ae4f3f8b8
George Onalo <onalotech7@gmail.com>
2023-03-31 06:17:55 +0100

    created nat-gateway module

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,11 @@
+# nat-gateway variables
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_id" {}
+variable "internet_gateway" {}
+variable "public_subnet_az2_id" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_data_subnet_az2_id" {}
